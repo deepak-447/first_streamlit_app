698795c4821a8c4cc5159af46888ec83e76e6c3b
deepak-447 <104711421+deepak-447@users.noreply.github.com>
2022-05-01 13:11:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
-streamlit.title('My Moms new healthy diner')
-                
-streamlit.title('Breakfast Menu')
+streamlit.title('My Moms new healthy diner')            
+streamlit.header('Breakfast Menu')
+streamlit.text('Poha')
+streamlit.text('Dosa')
+streamlit.text('Chila')
