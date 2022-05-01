caa9206fbd49def55783dbe6bef30422eace33f8
deepak-447 <104711421+deepak-447@users.noreply.github.com>
2022-05-01 13:12:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
-streamlit.title('My Moms new healthy diner')            
-streamlit.header('Breakfast Menu')
-streamlit.text('Poha')
-streamlit.text('Dosa')
-streamlit.text('Chila')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
