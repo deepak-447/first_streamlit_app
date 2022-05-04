1aa55ffee4d69acca64c075d0038865923e988c4
deepak-447 <104711421+deepak-447@users.noreply.github.com>
2022-05-04 18:40:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,53 +20,12 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
 
-#import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
-
-
-
-
-# Let's put a pick list here so they can pick the fruit they want to include
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-
-
-
-fruits_to_show = my_fruit_list.loc[fruits_selected]
-
-
-
-#display the table on the page
-streamlit.dataframe(fruits_to_show)
-
-
-
-
-
-
-
-
-
-
-
-#import snowflake.connector
-
-
-
-streamlit.header("The fruit load list contains:")
-#snowflake-related functions
-def get_fruit_load_list():
-with my_cnx.cursor() as my_cur:
-my_cur.execute("select* from fruit_load_list")
-return my_cur.fetchall()
-
-
-
-# add a button to load the fruit
-if streamlit.button('Get Fruit Load List'):
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_data_rows = get_fruit_load_list()
-streamlit.dataframe(my_data_rows)
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
 
 
 
